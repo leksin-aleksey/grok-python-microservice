cd7bf3d59c3537872a21b556c200332423b223d8
larjanka <yakovenko.maxim90@gmail.com>
2025-05-28 00:46:30 +0200

    Make a function in which I was included the API and Json

--- grok_python_microservice.py
+++ grok_python_microservice.py
@@ -1,29 +1,67 @@
 import requests
 import os
 
-# API Key from environment variable
-api_key = os.environ['grok_token']
+# # API Key from environment variable
+# api_key = os.environ['grok_token']
+#
+# # Endpoint Groq
+# url = "https://api.groq.com/openai/v1/chat/completions"
+#
+#
+# def grok_question(question: str) -> str:
+#      input("Write your question, please: ")
+#
+#
+#     # Body of the request
+#     payload = {
+#         "model": "llama3-70b-8192",  # Одна из доступных моделей Groq
+#         "messages": [
+#             {"role": "user", "content": {}}
+#         ],
+#         "temperature": 0.7
+#     }
+#
+#     # Headers
+#     headers = {
+#         "Authorization": f"Bearer {api_key}",
+#         "Content-Type": "application/json"
+#     }
+#
+#     # sending request
+#     response = requests.post(url, json=payload, headers=headers)
+#
+#     # Response answer
+#     print(response.json()["choices"][0]["message"]["content"])
 
-# Endpoint Groq
-url = "https://api.groq.com/openai/v1/chat/completions"
 
-# Body of the request
-payload = {
-    "model": "llama3-70b-8192",  # Одна из доступных моделей Groq
-    "messages": [
-        {"role": "user", "content": "Hi, could you send me the recipe of cocktails with beer?"}
-    ],
-    "temperature": 0.7
-}
+def grok_question(question: str) -> str:
+    # API Key from environment variable
+    api_key = os.environ['grok_token']
 
-# Headers
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Content-Type": "application/json"
-}
+    # Endpoint Groq
+    url = "https://api.groq.com/openai/v1/chat/completions"
 
-# sending request
-response = requests.post(url, json=payload, headers=headers)
+    # Body of the request
+    payload = {
+        "model": "llama3-70b-8192",  # Одна из доступных моделей Groq
+        "messages": [
+            {"role": "user", "content": question}
+        ],
+        "temperature": 0.7
+    }
 
-# Response answer
-print(response.json()["choices"][0]["message"]["content"])
+    # Headers
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+
+    # sending request
+    response = requests.post(url, json=payload, headers=headers)
+
+    # Response answer
+    print(response.json()["choices"][0]["message"]["content"])
+
+
+
+
