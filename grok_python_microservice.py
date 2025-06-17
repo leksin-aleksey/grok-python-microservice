c43e2ebde2b15555c05868f173e899051d3609c3
larjanka <yakovenko.maxim90@gmail.com>
2025-06-17 14:36:06 +0200

    Main updated max (#6)
    * Make a function in which I was included the API and Json
    
    * Make a function in which I was included the API and Json
    
    * Make a function in which I was included the API and Json
    
    * Make a function in which I was included the API and Json
    
    * Make a function in which I was included the API and Json
    
    * Make a function in which I was included the API and Json
    
    * remove unnecessary comments on lines 4-34
    
    * remove unnecessary comments on lines 4-34
    
     Add new file

--- grok_python_microservice.py
+++ grok_python_microservice.py
@@ -1,29 +1,31 @@
 import requests
 import os
 
-# API Key from environment variable
-api_key = os.environ['grok_token']
 
-# Endpoint Groq
-url = "https://api.groq.com/openai/v1/chat/completions"
+def grok_question(question: str) -> str:
+    # API Key from environment variable
+    api_key = os.environ['grok_token']
 
-# Body of the request
-payload = {
-    "model": "llama3-70b-8192",  # Одна из доступных моделей Groq
-    "messages": [
-        {"role": "user", "content": "Hi, could you send me the recipe of cocktails with beer?"}
-    ],
-    "temperature": 0.7
-}
+    # Endpoint Groq
+    url = "https://api.groq.com/openai/v1/chat/completions"
 
-# Headers
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Content-Type": "application/json"
-}
+    # Body of the request
+    payload = {
+        "model": "llama3-70b-8192",  # Одна из доступных моделей Groq
+        "messages": [
+            {"role": "user", "content": question}
+        ],
+        "temperature": 0.7
+    }
 
-# sending request
-response = requests.post(url, json=payload, headers=headers)
+    # Headers
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
 
-# Response answer
-print(response.json()["choices"][0]["message"]["content"])
+    # sending request
+    response = requests.post(url, json=payload, headers=headers)
+
+    # Response answer
+    return response.json()["choices"][0]["message"]["content"]
